d9eae148d2942ff4e10c7ed35043637318d65ec2
claudiarichardxx <claudiarichardxx@gmail.com>
2025-03-13 00:12:49 -0400

    Initial portfolio setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My Portfolio</h1>
+        <p>Welcome to my personal portfolio site!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
